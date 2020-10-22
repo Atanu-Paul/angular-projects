83e1609e676465c6bb968e2df4ace23f101b11c0
Atanu Paul <atanu.paul.rims@gmail.com>
2020-10-22 11:53:59 +0530

    third push

--- passwordGenaretorApp/src/app/app.component.ts
+++ passwordGenaretorApp/src/app/app.component.ts
@@ -7,7 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'passwordGenaretorApp';
+  //defining a password property and setting it to an empty string
+  password = '';
+
   onButtonClick() {
     console.log(`Button was clicked`);
+    this.password = 'Password 1';
   }
+
+  // showName() {
+  //   return 'A NAME';
+  // }
 }
